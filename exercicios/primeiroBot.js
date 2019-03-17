96c4486febdfd464561981ddeb91e9a86b4153a6
Raphael Coelho <raphaelcoel@gmail.com>
2019-03-17 19:40:35 -0300

    primeiro commit

---
+++ exercicios/primeiroBot.js
@@ -0,0 +1,28 @@
+const env = require('../.env')
+const Telegraf = require('telegraf')
+const bot = new Telegraf(env.token)
+
+bot.start(ctx => {
+    const from = ctx.update.message.from
+    console.log(from)
+    ctx.reply(`Seja bem vindo, ${from.first_name}!`)
+
+})
+
+bot.startPolling()
+
+bot.on('text', async (ctx, next) => {
+    const first_name = ctx.update.message.from.first_name
+    if(first_name == 'Regina'){
+        await ctx.reply(`Oi o Raphael pediu pra te avisar que vc é o amor da vida dele ♥`)
+    }else{
+        await ctx.reply('Mid 1')
+    }
+   
+    next()
+})
+
+bot.on('text', async (ctx, next) => {
+    await ctx.reply('Mid 2')
+    next()
+})
